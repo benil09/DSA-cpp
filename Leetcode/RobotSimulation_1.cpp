ad0fd799b4cca58fe7039f2772d206fda1224a7b
benil09 <benil8235@gmail.com>
2026-04-07 18:42:24 +0530

    stash

---
+++ Leetcode/RobotSimulation_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   vector<int> cmd ={4,-1,3};
+
+   int Xc = 0 ;
+   int Yc = 0 ;
+
+   int dir = 0; // 0=N 1=E 2=S 3=W
+
+   for(int i = 0 ; i < cmd.size();i++){
+        if(cmd[i] == -1 ){
+            dir = (dir + 1) % 4; // turn right
+        }else if(cmd[i] == -2){
+            dir = (dir + 3) % 4; // turn left
+        }else{
+             int steps = cmd[i];
+            while (steps--) {
+                if (dir == 0) Yc++;
+                else if (dir == 1) Xc++;
+                else if (dir == 2) Yc--;
+                else if (dir == 3) Xc--;
+            }
+        }
+   }
+
+    cout << "Final Position: (" << Xc << ", " << Yc << ")" << endl;
+    return 0;
+}
